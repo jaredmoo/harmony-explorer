2cc76ce981419685edfd42d5dae6f28bd796ed91
Jared Moore <jared@vanessaandjared.com>
2024-12-01 22:41:38 -0800

    rename to chord labels.txt

--- program.py
+++ program.py
@@ -2,9 +2,9 @@ from scale import scale_index
 from chord_label import chord_label_index
 from relationship import relationships
 
-chord_label_index.dump("chords_all.txt")
+chord_label_index.dump("chord_labels_chromatic.txt")
 for s in scale_index.values():
-    chord_label_index.restrict(s).dump("chords_" + s.name + ".txt")
+    chord_label_index.restrict(s).dump("chord_labels_" + s.name + ".txt")
 
 with open("relationships.txt", "w", encoding="utf8") as f:
     f.truncate()
