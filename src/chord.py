4e963d64e9d6b2f978c33e9ae3120d8038e7b67a
Jared Moore <jared@vanessaandjared.com>
2024-12-03 21:23:50 -0800

    chord label index

--- src/chord.py
+++ src/chord.py
@@ -1,6 +1,7 @@
-from chord_label import ChordLabel
+from chord_label import ChordLabel, ChordLabelIndex
 from note import Note
 from prettify import prettify
+from typing import Iterable
 
 
 class Chord:
@@ -12,3 +13,20 @@ class Chord:
 
     def __repr__(self):
         return f"{prettify(self.name)} {self.notes}"
+
+
+class ChordIndex:
+    def __init__(self, root: Note, chord_label_index: ChordLabelIndex):
+        self.root = root
+        self._by_name: dict[str, Chord] = dict()
+        # self._by_notes = dict()
+        for cl in chord_label_index.values():
+            c = Chord(self.root, cl)
+            self._by_name[c.name] = c
+            # _by_notes[c.notes] = c # this is not going to work right now because eq & hash aren't implemented
+
+    def values(self) -> Iterable[Chord]:
+        return self._by_name.values()
+
+    def by_name(self, name: str):
+        return self._by_name[name]
